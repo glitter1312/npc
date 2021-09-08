ff3b0260296a7a618b1794c08d7ccf1e839343a2
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-09-07 23:46:02 -0700

    first commit

---
+++ src/pets.js
@@ -0,0 +1,22 @@
+export const accounts = {
+  glitter: [
+    pet('Megan the Royalgirl Zafara'),
+    pet('Graham the Royalboy Zafara'),
+    pet('Marissa the Royalgirl Kyrii'),
+    pet('Nacho the Baby Xweetok'),
+    pet('Pyjamas the Baby Skeith'),
+    pet('Babe the Baby Usul'),
+    pet('Quinn the Striped Xweetok'),
+    pet('Sourpuss the Darigan Wocky'),
+    pet('Ella the Darigan Aisha'),
+    pet('Janie the Plushie Zafara'),
+    pet('Maisie the Plushie Cybunny'),
+    pet('Hashbrown the Plushie Aisha'),
+    pet('Pancakes the Faerie Blumaroo'),
+    pet('Pancakes the Faerie Blumaroo'),
+    pet('Carly the Faerie Ixi'),
+    pet('Pout the Grey Aisha'),
+    pet('Applesauce the Island Aisha'),
+    pet('Salami the Checkered Aisha'),
+  ]
+}
